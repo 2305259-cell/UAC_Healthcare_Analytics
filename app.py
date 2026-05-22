3cf37c21e56cc450610929a2390cc7225018535e
Sweta padma patra <2305259@kiit.ac.in>
2026-05-22 11:12:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,13 @@ def load_data():
     ]
 
     # Convert date
-    df['Date'] = pd.to_datetime(df['Date'])
+   df['Date'] = pd.to_datetime(
+    df['Date'],
+    errors='coerce'
+)
+
+# Remove invalid dates
+df = df.dropna(subset=['Date'])
 
     # Convert numeric columns
     numeric_cols = [
